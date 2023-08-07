67c5654fd4acd6770e29cdc217ab761ca5a759c9
Sean Krueger <skrueger2270@gmail.com>
2023-08-07 00:12:26 -0500

    Set send to non-blocking

--- worker/stdout_stream.py
+++ worker/stdout_stream.py
@@ -3,6 +3,7 @@ import socket
 class Sender():
     def __init__(self, addr, worker_id):
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.setblocking(False)
         self.addr = addr
         self.worker_id = bytes(str(worker_id).rjust(3, "0").encode())
     
