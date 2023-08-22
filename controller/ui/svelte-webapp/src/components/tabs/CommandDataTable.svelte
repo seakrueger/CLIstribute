4399b8fcb452a64ebe04931fe335c0ad475d814a
Sean Krueger <skrueger2270@gmail.com>
2023-08-22 13:05:07 -0600

    Clean up table a little

--- controller/ui/svelte-webapp/src/components/tabs/CommandDataTable.svelte
+++ controller/ui/svelte-webapp/src/components/tabs/CommandDataTable.svelte
@@ -28,10 +28,10 @@
     <tbody>
         {#each data as item}
             <tr value={item.status}>
-                <td>{item.job_id}</td>
-                <td>{item.cmd}</td>
-                <td>{item.status}</td>
-                <td>{item.capture_stdout}</td>
+                <td id=id>{item.job_id}</td>
+                <td id=cmd>{item.cmd}</td>
+                <td id=status>{item.status}</td>
+                <td id=capture>{item.capture_stdout === 1 ? '✓' : "✗"}</td>
             </tr>        
         {/each}
     </tbody>
@@ -53,7 +53,7 @@
     }
 
     tr[value="failed"] {
-        background-color: firebrick;
+        background-color: orangered;
     }
 
     tr[value="running"] {
@@ -62,5 +62,23 @@
 
     tr[value="starting"] {
         background-color: aqua;
+    }
+
+    #id {
+        width: 5%;
+    }
+
+    #cmd {
+        width: 80%;
+        text-align: left;
+        padding-left: 5px;
+    }
+
+    #status {
+        width: 10%;
+    }
+
+    #capture {
+        width: 5%;
     }
 </style>
